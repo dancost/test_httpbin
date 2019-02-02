a04cfaed17373f67325cfbbc1bf6060d480e2c23
Erux <danutzelu@gmail.com>
2019-02-02 20:30:04 +0200

    remove some irelevant func params

--- test_httpbin.py
+++ test_httpbin.py
@@ -8,7 +8,7 @@ url = "https://httpbin.org/get"
 
 
 # define get request function.
-def get_request(url, method = 'GET', args=''):
+def get_request(url, method='GET', args=''):
     if method == 'GET':
         r = requests.get(url + args)
 
@@ -27,12 +27,12 @@ def test_get_response_200():
 
 
 # 2 check route returns 200 status with valid arguments
-def test_get_with_args(args=args):
+def test_get_with_args():
     assert get_request(url=url, args=args).status_code == 200
 
 
-# 3 check reponse args are correctly formatted
-def test_get_args_content_format(args=args):
+# 3 check response args are correctly formatted
+def test_get_args_content_format():
     response = get_request(url=url, args=args).json()
     assert response['args'] == {
         "drinks": "fanta,nestea",
@@ -44,7 +44,7 @@ def test_get_args_content_format(args=args):
 
 
 # 4 check response format with tuple format arguments in request
-def test_get_with_tuple_args(args=tuple_args):
+def test_get_with_tuple_args():
     response = get_request(url=url, args=args).json()
     assert response['args'] == {"fruits": "(\"apple\",\"banana\",\"cherry\")"}
 
@@ -59,4 +59,4 @@ def test_route_allowed_methods():
 
 # 6 Check forbidden method returns correct status code
 def test_method_not_allowed():
-    assert get_request(url=url, method = 'POST').status_code == 405
+    assert get_request(url=url, method='POST').status_code == 405
