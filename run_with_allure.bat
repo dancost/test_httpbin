7699c70bdeebcb866ed3ebf39850774637d197a2
Erux <danutzelu@gmail.com>
2019-02-03 12:49:47 +0200

    added install steps for test run prequisites

--- run_with_allure.bat
+++ run_with_allure.bat
@@ -1,2 +1,19 @@
+@echo off
+echo Installing scoop
+powershell -nop -c "iex (new-object net.webclient).downloadstring('https://get.scoop.sh')"
+echo Checking for Allure installation
+if exist %USERPROFILE%\scoop\shims\allure.cmd echo Allure installed. Skipping! & goto hasScoop 
+
+
+echo Using scoop to install allure
+scoop install allure
+
+
+:hasScoop
+@echo off
+echo Installing python prequisites
+pip install -r requirements.txt
+echo Running tests
 py.test test_httpbin.py -vv --alluredir=./reports
 allure serve .\reports\
+pause
